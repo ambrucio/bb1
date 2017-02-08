5e70c3290318a2f4137535f282b897c6b79611fe
“ambrucio” <ambrucio@gmail.com>
2017-02-08 17:00:55 +0200

    Reshenie zadach

--- zadachi.php
+++ zadachi.php
@@ -2,7 +2,7 @@
 /* задача 1*/
     $name='Max';
     $s_name='BB';
-    $age='34';
+    $age=34;
     echo 'Меня зовут : '.$name ; ?> <br>
 <?php
     echo "Меня зовут : $name" ; ?> <br>
@@ -21,6 +21,33 @@
     echo $name;  /*дважды выводит имя*/ ?> <br>
 <?php
 
+/* Массивы*/
+$employee1=['Max', 'Vika'];
+echo $employee1 [1];
 
+$employee2=['Riki', 'Nika'];
+echo $employee2 [0];
+
+$employees=[$employee1, $employee2]; ?> <br>
+<?php
+echo '<pre>';
+print_r($employees);
+echo '</pre>';                       ?> <br>
+
+
+<?php
+    $age='fgh';
+
+    if ($age>=18 && $age<=59){
+        echo 'Вам еще работать и работать';
+    }
+    if ($age>59){
+        echo 'Вам пора на пенсию';
+    }
+    if ($age >= 0 && $age <= 17) {
+        echo 'Вам еще рано работать';}
+
+    if ($age < 0) {
+        echo 'Неизвестный возраст';}
 
 
