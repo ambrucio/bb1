a161e6a797be19e1ff5d4f608a65297ebce7ce10
“ambrucio” <ambrucio@gmail.com>
2017-02-12 17:24:00 +0200

    Izm

--- php_basics_tasks/8.php
+++ php_basics_tasks/8.php
@@ -2,26 +2,31 @@
 /*Расширьте конструкцию if из п.5-7, выводя фразу: "Неизвестный возраст" при условии, что значение переменной
 age является отрицательным числом, или вовсе числом не является*/
 
-$age=0;
+$age=3;
+$fraza1='Неизвестный возраст';
+$fraza2='Вам еще работать и работать';
+$fraza3='Вам пора на пенсию';
+$fraza4='Вам еще рано работать';
 
-if ($age !== (int)$age) {
-    echo 'Неизвестный возраст';
-}
-elseif ($age >= 18 and $age <= 59){
-    echo 'Вам еще работать и работать';
-}
-elseif ($age>59){
-    echo 'Вам пора на пенсию';
-}
-elseif ($age > 0 and $age <= 17){
-    echo 'Вам еще рано работать';
-}
-elseif ($age === 0){
-    echo 'Вам еще рано работать';
-}
-elseif ($age < 0 ){
-    echo 'Неизвестный возраст';
-}
-else {
-    echo'Неизвестный возраст';
-}
+
+    if ($age !== (int)$age) {
+    echo $fraza1;
+    }
+        elseif ($age >= 18 and $age <= 59){
+        echo $fraza2;
+    }
+        elseif ($age>59){
+        echo $fraza3;
+    }
+        elseif ($age > 0 and $age <= 17){
+        echo $fraza4;
+    }
+        elseif ($age === 0){
+        echo $fraza4;
+    }
+        elseif ($age < 0 ){
+        echo $fraza1;
+    }
+        else {
+        echo $fraza1;
+    }
