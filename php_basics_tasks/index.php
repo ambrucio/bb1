8ff29870d29f0d6eba10c8ff4167391efddcd155
“ambrucio” <ambrucio@gmail.com>
2017-02-13 11:16:55 +0200

    php_DZ_arrays

--- php_basics_tasks/index.php
+++ php_basics_tasks/index.php
@@ -1,35 +1,40 @@
+<!DOCTYPE html>
 <head>
+    <meta charset="UTF-8">
+    <title>Задачи по основам PHP</title>
     <style>
-        p {background: #f0f0f0;
+        p {text-align: center;
+            background: #f0f0f0;
         }
     </style>
 </head>
-    <body>
-        <p><a href='./1.php'>Task 1</a></p>
-        <p><a href='./2.php'>Task 2</a></p>
-        <p><a href='./3.php'>Task 3</a></p>
-        <p><a href='./4.php'>Task 4</a></p>
-        <p><a href='./5.php'>Task 5</a></p>
-        <p><a href='./6.php'>Task 6</a></p>
-        <p><a href='./7.php'>Task 7</a></p>
-        <p><a href='./8.php'>Task 8</a></p>
-        <p><a href='./9.php'>Task 9</a></p>
-        <p><a href='./10.php'>Task 10</a></p>
-        <p><a href='./11.php'>Task 11</a></p>
-        <p><a href='./12.php'>Task 12</a></p>
-        <p><a href='./13.php'>Task 13</a></p>
-        <p><a href='./14.php'>Task 14</a></p>
-        <p><a href='./15.php'>Task 15</a></p>
-        <p><a href='./16.php'>Task 16</a></p>
-        <p><a href='./17.php'>Task 17</a></p>
-        <p><a href='./18.php'>Task 18</a></p>
-        <p><a href='./19.php'>Task 19</a></p>
-        <p><a href='./20.php'>Task 20</a></p>
-        <p><a href='./21.php'>Task 21</a></p>
-        <p><a href='./22.php'>Task 22</a></p>
-        <p><a href='./23.php'>Task 23</a></p>
-        <p><a href='./24.php'>Task 24</a></p>
-        <p><a href='./25.php'>Task 25</a></p>
-        <p><a href='./26.php'>Task 26</a></p>
-    </body>
+<body>
+<p><a href='./1.php'>Task 1</a></p>
+<p><a href='./2.php'>Task 2</a></p>
+<p><a href='./3.php'>Task 3</a></p>
+<p><a href='./4.php'>Task 4</a></p>
+<p><a href='./5.php'>Task 5</a></p>
+<p><a href='./6.php'>Task 6</a></p>
+<p><a href='./7.php'>Task 7</a></p>
+<p><a href='./8.php'>Task 8</a></p>
+<p><a href='./9.php'>Task 9</a></p>
+<p><a href='./10.php'>Task 10</a></p>
+<p><a href='./11.php'>Task 11</a></p>
+<p><a href='./12.php'>Task 12</a></p>
+<p><a href='./13.php'>Task 13</a></p>
+<p><a href='./14.php'>Task 14</a></p>
+<p><a href='./15.php'>Task 15</a></p>
+<p><a href='./16.php'>Task 16</a></p>
+<p><a href='./17.php'>Task 17</a></p>
+<p><a href='./18.php'>Task 18</a></p>
+<p><a href='./19.php'>Task 19</a></p>
+<p><a href='./20.php'>Task 20</a></p>
+<p><a href='./21.php'>Task 21</a></p>
+<p><a href='./22.php'>Task 22</a></p>
+<p><a href='./23.php'>Task 23</a></p>
+<p><a href='./24.php'>Task 24</a></p>
+<p><a href='./25.php'>Task 25</a></p>
+<p><a href='./26.php'>Task 26</a></p>
+<p><a href='./27.php'>Task 27</a></p>
+</body>
 
