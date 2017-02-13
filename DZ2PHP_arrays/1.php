94cab7702afe070d0795748d4566114a33ac7b04
“ambrucio” <ambrucio@gmail.com>
2017-02-13 13:48:15 +0200

    Nachalo

---
+++ DZ2PHP_arrays/1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mazur
+ * Date: 13.02.2017
+ * Time: 13:46
+ */
---
+++ DZ2PHP_arrays/DZ2PHP_arrays.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Задачи по массивам и циклам</title>
+    <style>
+p {text-align: center;
+            background: #f0f0f0;
+        }
+           p {text-align: center;
+              background: #f0f0f0;
+           }
+    </style>
+</head>
+<body>
+<p><a href='./1.php'>Task 1</a></p>
+
+</body>
