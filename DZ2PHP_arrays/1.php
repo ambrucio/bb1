353da0511ef183ae6103d76ed1d5229a006ac10c
“ambrucio” <ambrucio@gmail.com>
2017-02-13 16:08:50 +0200

    izm1

--- DZ2PHP_arrays/1.php
+++ DZ2PHP_arrays/1.php
@@ -1,6 +1,8 @@
 <?php
+/*Дан массив с элементами 'html', 'css', 'php', 'js', 'jq'. С помощью цикла foreach выведите эти слова в столбик.*/
+
 $massiv=[1=>'html', 2=> 'css', 3=> 'php', 4=> 'js', 5=>'jq'];
 
 foreach ($massiv as $value => $key) {
-    echo "$value $key";
+    echo "<a>$value $key</a><br>";
 }
