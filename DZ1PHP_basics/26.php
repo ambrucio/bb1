cc2344ee6d02ab7a3b1fa4fd0d69f49c84ec50b6
“ambrucio” <ambrucio@gmail.com>
2017-02-14 13:50:07 +0200

    posle proverki

--- DZ1PHP_basics/26.php
+++ DZ1PHP_basics/26.php
@@ -2,4 +2,4 @@
 /*Объявите константу DAYS_COUNT равную 7 и константу MONTH_COUNT равную 12 двумя разными способами объявления констант*/
 
 define ("DAYS_COUNT", 7);
-define ("MONTH_COUNT", 12, true);
+const MONTH_COUNT=12;
