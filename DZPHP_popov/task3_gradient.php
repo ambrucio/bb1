045a4c03f243e7872367dcccf0b35deb8c181e77
“ambrucio” <ambrucio@gmail.com>
2017-02-23 21:41:41 +0200

    rename

--- DZPHP_popov/task3_gradient.php
+++ DZPHP_popov/task3_gradient.php
@@ -4,6 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Генератор случайных таблиц</title>
+<style type="text/css">
+
+    h1, h3 {text-align : center; }
+
+    </style>
+
+
 </head>
 <body>
 
@@ -11,19 +18,18 @@
     <h3>Make gradients from left to right on each row</h3>
     <?php
 
-
     $rows = 10;
     $cols = 10;
 
-    $color_value = range (16756000, 16766000, 100);
+    $color_value = range (16756000, 16756200, 2);
 
 
-var_dump($color_value);
+//var_dump($color_value);
 
 
     ?>
 
-<table border="1" cellpadding="2" cellspacing="0">
+<table align="center" border="1" cellpadding="2" cellspacing="0">
 
     <?php for ($i = 1; $i <= $rows; $i++): ?>
             <tr>
