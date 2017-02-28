4f7102b7aace69825767c5f1a289e9e16345816b
“ambrucio” <ambrucio@gmail.com>
2017-02-28 16:11:51 +0200

    izm

--- DZ3PHP_funfor/1.php
+++ DZ3PHP_funfor/1.php
@@ -5,35 +5,54 @@
  общими словами.
  */
 
-include '1.html';
+/*include '1.html';*/
 
 $a = $_POST ['words_a'];
 $b = $_POST ['words_b'];
-
+$arr = [];
 function getCommonWords($a, $b)
 {
-    $a = explode (' ',$a);
-    $b = explode (' ',$b);
+
+    $a = explode(' ', $a);
+    $b = explode(' ', $b);
 
     for ($i = 0; $i <= count($a); $i++)
-    {   //$test = false;
-        for ($j = 0; $j <= count($b); $j++) {
+    {
+        for ($j = 0; $j <= count($b); $j++)
+        {
 
             if ($a[$i] === $b[$j])
 
-                $arr[] = $a[$i];
-            //$test=true;
-            break;
-            /*}
-                if($test==false){
-                    echo "Строка а(".$a[$i].") не совпала";
-                }*/
+                $arr [ ] = $a[$i];
         }
-    } return $arr;
+
+    }
+    return $arr;
 }
-print_r(getCommonWords($a, $b));
+
+
+/*var_dump($b[$j]);
+var_dump(getCommonWords($a, $b));*/
+
+//echo implode (', ',getCommonWords($a, $b));
 
 ?>
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>task 1</title>
+</head>
+<body>
 
+<form method="post">
+    <textarea rows="10" cols="45" name="words_a" placeholder="Ваше сообщение 1" ><?=getCommonWords('a')?></textarea>
+    <textarea rows="10" cols="45" name="words_b" placeholder="Ваше сообщение 2"></textarea>
+    <div><button>Отправить</button></div>
+    <div><h3><?=implode (', ',getCommonWords($a, $b));?></h3></div>
+</form>
+
+</body>
+</html>
 
