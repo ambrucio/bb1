4c691986e85fd9a81e38a2deb3d2b865a33d370e
“ambrucio” <ambrucio@gmail.com>
2017-03-03 16:51:01 +0200

    izm

--- DZ3PHP_funfor/2.php
+++ DZ3PHP_funfor/2.php
@@ -3,15 +3,6 @@
 в тексте. Реализовать с помощью функции.*/
 
 
-function my_array_reverse($arr)
-{
-    for($i = count($arr); $i < 0 ; $i--)
-    {
-        $result[] = $array[$i];
-    }
-    return $result;
-}
-var_dump(my_array_reverse(array(1, 2, 3, 4)));
 
 
 
